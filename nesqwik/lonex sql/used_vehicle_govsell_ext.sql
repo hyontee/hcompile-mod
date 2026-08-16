3725c3623a9db35673d30610eda10133a587a02d
root <root@hyonte.com>
2026-08-16 11:50:50 +0000

    Clean repository 2026-08-16 11:50:50

--- nesqwik/lonex sql/used_vehicle_govsell_ext.sql
+++
@@ -1,7 +0,0 @@
--- Русифицированный патч /sellcar: пробег, износ и страховка личного авто.
--- Названия колонок оставлены английскими, потому что они используются в Pawn-коде.
-
-ALTER TABLE `ownable_cars`
-    ADD COLUMN `car_wear` TINYINT UNSIGNED NOT NULL DEFAULT 0,
-    ADD COLUMN `insurance_until` INT NOT NULL DEFAULT 0,
-    ADD COLUMN `insurance_claims` SMALLINT UNSIGNED NOT NULL DEFAULT 0;
