d8bf40cc3613bb53215b2223eea07485ae1ab453
root <root@hyonte.com>
2026-09-08 06:03:52 +0000

    Auto-build 2026-09-08 06:03:52

---
+++ яяябананбанан/gamemodes/sile/.tmp_radialmenu/radialmenuforcar/utils/RadialMenuUseful.java
@@ -0,0 +1,23 @@
+package com.blackhub.bronline.game.gui.radialmenuforcar.utils;
+
+import androidx.compose.runtime.internal.StabilityInferred;
+import kotlin.Metadata;
+import org.apache.commons.compress.harmony.unpack200.SegmentConstantPool;
+import org.jetbrains.annotations.NotNull;
+
+/* compiled from: RadialMenuUseful.kt */
+@StabilityInferred(parameters = 1)
+@Metadata(d1 = {"\u0000\u0014\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0003\n\u0002\u0010\u000e\n\u0002\b\u0002\bÇ\u0002\u0018\u00002\u00020\u0001B\t\b\u0002¢\u0006\u0004\b\u0002\u0010\u0003R\u000e\u0010\u0004\u001a\u00020\u0005X\u0086T¢\u0006\u0002\n\u0000R\u000e\u0010\u0006\u001a\u00020\u0005X\u0086T¢\u0006\u0002\n\u0000¨\u0006\u0007"}, d2 = {"Lcom/blackhub/bronline/game/gui/radialmenuforcar/utils/RadialMenuUseful;", "", SegmentConstantPool.INITSTRING, "()V", "KEY_GET_DATA", "", "KEY_ACTION_TYPE", "app_siteRelease"}, k = 1, mv = {2, 2, 0}, xi = 48)
+/* loaded from: classes4.dex */
+public final class RadialMenuUseful {
+    public static final int $stable = 0;
+
+    @NotNull
+    public static final RadialMenuUseful INSTANCE = new RadialMenuUseful();
+
+    @NotNull
+    public static final String KEY_ACTION_TYPE = "t";
+
+    @NotNull
+    public static final String KEY_GET_DATA = "s";
+}
